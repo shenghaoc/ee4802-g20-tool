2f236c42673a3344089664ffcba0b9e816e42dde
shenghaoc <shenghaoc@outlook.com>
2024-01-15 03:15:12 +0800

    Use POST with validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+
+import { ml_model_list } from './lists';
+import { town_list } from './lists';
+import { storey_range_list } from './lists';
+import { flat_model_list } from './lists';
 
 type Bindings = {
   DB: D1Database;
@@ -8,19 +15,29 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 app.use('/api/*', cors());
 
-app.get('/api/prices', async c => {
-  const { ml_model, month_start, month_end, town, storey_range, flat_model, floor_area_sqm, lease_commence_date } = c.req.query();
+app.post(
+  '/api/prices',
+  zValidator(
+    'form',
+    z.object({
+      ml_model: z.enum(ml_model_list),
+      town: z.enum(town_list),
+      storey_range: z.enum(storey_range_list),
+      flat_model: z.enum(flat_model_list,),
+      floor_area_sqm: z.string().transform((val) => parseFloat(val))
+        .pipe(z.number().min(0, 'Must be greater than 0')),
+      lease_commence_date: z.string().transform((val) => new Date(val))
+        .pipe(z.date().min(new Date('1960-01-01'), { message: 'Must not be before 1960' })
+          .max(new Date('2022-02-01'), { message: 'Must not be in future' }))
+    })
+  ),
+  async (c) => {
+    let formData: FormData;
+    await c.req.parseBody().then(result => {
+      formData = result;
+    });
 
-  if (!ml_model) return c.text('Missing ML Model');
-  if (!month_start) return c.text('Missing Start Month');
-  if (!month_end) return c.text('Missing End Month');
-  if (!town) return c.text('Missing Town');
-  if (!storey_range) return c.text('Missing Storey Range');
-  if (!flat_model) return c.text('Missing Flat Model');
-  if (!floor_area_sqm) return c.text('Missing Floor Area');
-  if (!lease_commence_date) return c.text('Missing Lease Commence Date');
-
-  const { results } = await c.env.DB.prepare("SELECT intercept_map, month_map, \
+    const { results } = await c.env.DB.prepare("SELECT intercept_map, month_map, \
   storey_range_map, floor_area_sqm_map, lease_commence_date_map, \
   months_ordinal.name \
   AS month_name, \
@@ -46,20 +63,20 @@ app.get('/api/prices', async c => {
   BETWEEN ?4 \
   AND ?5 \
   AND storey_ranges_ordinal.name=?6;")
-    .bind(ml_model, town, flat_model, month_start, month_end, storey_range)
-    .all();
+      .bind(formData.ml_model, formData.town, formData.flat_model, formData.month_start, formData.month_end, formData.storey_range)
+      .all();
 
-  return c.json(results.map(x => ({
-    labels: x["month_name"], data: (Math.round(Math.max(0, (x["intercept_map"]
-      + x["month_multiplier"] * x["month_map"]
-      + x["town_map"]
-      + x["storey_range_multiplier"] * x["storey_range_map"]
-      + floor_area_sqm * x["floor_area_sqm_map"]
-      + x["flat_model_map"]
-      + lease_commence_date * x["lease_commence_date_map"])) * 100) / 100
-    )
-  })));
-});
+    return c.json(results.map(x => ({
+      labels: x["month_name"], data: (Math.round(Math.max(0, (x["intercept_map"]
+        + x["month_multiplier"] * x["month_map"]
+        + x["town_map"]
+        + x["storey_range_multiplier"] * x["storey_range_map"]
+        + formData.floor_area_sqm * x["floor_area_sqm_map"]
+        + x["flat_model_map"]
+        + formData.lease_commence_date * x["lease_commence_date_map"])) * 100) / 100
+      )
+    })));
+  });
 
 app.onError((err, c) => {
   console.error(`${err}`);
